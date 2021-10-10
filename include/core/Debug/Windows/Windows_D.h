e743cf02d06dcbb2d7170bc255fd432360deb28b
ngomez <naimgomezcn@gmail.com>
2021-10-10 18:15:29 +0200

    Working on it

--- include/core/Debug/Windows/Windows_D.h
+++ include/core/Debug/Windows/Windows_D.h
@@ -1,3 +1,16 @@
+/**
+ * @file Windows_D.h
+ * @author ngomez
+ * @date 90/10/2021
+ * @brief File containing example of doxygen usage for quick reference.
+ *
+ * Here typically goes a more extensive explanation of what the header
+ * defines. Doxygens tags are words preceeded by either a backslash @\
+ * or by an at symbol @@.
+ * @see http://www.stack.nl/~dimitri/doxygen/docblocks.html
+ * @see http://www.stack.nl/~dimitri/doxygen/commands.html
+ */
+
 #pragma once
 #include <iostream>
 
@@ -19,13 +32,63 @@ namespace mlp
 {
     namespace debug
     {
+        /**
+         * @brief Library for debugging open operating system windows
+         * 
+         * @code
+         * // Initialization example
+         * MLP::debug::Windows_D wDebug();
+         * @endcode
+         * 
+         * @warning  Only Windows 7 and above supported
+         */
         class Windows_D
         {
 
         public:
-                std::vector<std::wstring> getList(std::wstring separator = L"\n");
-                void printList(bool numerated=false);
-                bool isThisWindow(std::wstring tWindow);
+            
+                /**
+                   * @brief Returns a list containing all open windows.
+                   * 
+                   * @code
+                   *  // usage example
+                   *  MLP::debug::Windows_D wDebug();
+                   *  wDebug.getList(L"\n");                   * 
+                   * @endcode
+                   * 
+                   * @param param1 Optional parameter to determine if you want to add some character at the end of each element, by default it is (L ",")
+                   * @return std::vector<std::wstring>
+                   */
+                std::vector<std::wstring> getList(std::wstring separator = L",");
+
+                /**
+                   * @brief Print all open windows.
+                   *
+                   * @code
+                   *  // usage example
+                   *  MLP::debug::Windows_D wDebug();
+                   *  wDebug.printList(true);                   
+                   * @endcode
+                   *
+                   * @param param1 Optional parameter, true to enumarte windows [0..N], by default is false
+                   */
+                void printList(bool numerated=false); 
+
+                /**
+                   * @brief Check if window is opened.
+                   *
+                   * 
+                   * @code
+                   *  // usage example
+                   *  MLP::debug::Windows_D wDebug();
+                   *  bool isOpen wDebug.isOpen(L"Spotify");
+                   * @endcode
+                   *
+                   * @param param1 Get the name of the window to check as a parameter
+                   * @warning The name must be the same to the window to search. (character by character)
+                   * @return bool
+                   */
+                bool isOpen(std::wstring tWindow); 
         };     
     }
 }
