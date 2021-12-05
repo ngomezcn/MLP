90265716b606be332696b80bdaaca1c593f5e2f2
ngomezKS <naimgomezcn@gmail.com>
2021-12-05 23:39:33 +0100

    remove usseles thing

--- include/core/Debug/Windows/Windows_D.h
+++ include/core/Debug/Windows/Windows_D.h
@@ -1,14 +1,8 @@
 /**
  * @file Windows_D.h
  * @author ngomez
- * @date 90/10/2021
- * @brief File containing example of doxygen usage for quick reference.
+ * @date 09/10/2021
  *
- * Here typically goes a more extensive explanation of what the header
- * defines. Doxygens tags are words preceeded by either a backslash @\
- * or by an at symbol @@.
- * @see http://www.stack.nl/~dimitri/doxygen/docblocks.html
- * @see http://www.stack.nl/~dimitri/doxygen/commands.html
  */
 
 #pragma once
